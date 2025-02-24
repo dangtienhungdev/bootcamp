8e3270d0cc0120173f032189b119ae4040aae4e6
dangtienhung <hungdang02042003@gmail.com>
2025-02-24 22:07:49 +0700

    buổi 36: end app music

--- less32/assets/js/app.js
+++ less32/assets/js/app.js
@@ -85,6 +85,7 @@ function clicked(liIndex) {
 	const currentMusic = musicAll[liIndex];
 	loadMusic(currentMusic);
 	playingSong();
+	playMusic();
 }
 
 const litags = wrapper.querySelectorAll('li');
@@ -108,9 +109,17 @@ function playingSong() {
 }
 
 const playMusic = () => {
+	playPauseBtn.classList.add('paused');
 	playPauseBtn.innerHTML = `<i class="fa-solid fa-pause"></i>`;
 	musicImage.classList.add('rotate');
 	mAudio.play();
+};
+
+const pauseMusic = () => {
+	playPauseBtn.classList.remove('paused');
+	playPauseBtn.innerHTML = `<i class="fa-solid fa-play"></i>`;
+	musicImage.classList.remove('rotate');
+	mAudio.pause();
 };
 
 // logic phát nhạc
@@ -154,6 +163,42 @@ mAudio.addEventListener('timeupdate', (e) => {
 });
 
 playPauseBtn.addEventListener('click', () => {
+	if (playPauseBtn.classList.contains('paused')) {
+		// handle logic stop music
+		pauseMusic();
+	} else {
+		// phats nhạc
+		playMusic();
+	}
+});
+
+nextBtn.addEventListener('click', () => {
+	musicIndex++;
+	if (musicIndex > musicAll.length - 1) {
+		musicIndex = 0;
+	}
+	const currentMusic = musicAll[musicIndex];
+	loadMusic(currentMusic);
+	playMusic();
+});
+
+prevBtn.addEventListener('click', () => {
+	musicIndex--;
+	if (musicIndex < 0) {
+		musicIndex = musicAll.length - 1;
+	}
+	const currentMusic = musicAll[musicIndex];
+	loadMusic(currentMusic);
+	// playingSong();
+	playMusic();
+});
+
+progressArea.addEventListener('click', (e) => {
+	const progressWidth = progressArea.clientWidth;
+	const offsetX = e.offsetX;
+	const songDuration = mAudio.duration;
+
+	mAudio.currentTime = (offsetX / progressWidth) * songDuration;
 	playMusic();
 });
 
