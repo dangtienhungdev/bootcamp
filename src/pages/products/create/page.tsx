a034f6ecc25dac40d667300016b4c5954583f929
Dang Tien Hung <hungdang02042003@gmail.com>
2025-09-16 20:59:19 +0700

    feat: enhance create product form with category selection and improved input fields

--- src/pages/products/create/page.tsx
+++ src/pages/products/create/page.tsx
@@ -1,14 +1,43 @@
-import { Form, Input, InputNumber, Switch, Button, Row, Col } from 'antd'
+import { Button, Col, Form, Input, InputNumber, Row, Select, Switch } from 'antd'
+import { useCallback, useMemo, useState } from 'react'
+
+import { useGetCategoriesQuery } from '@/services/category.service'
+import { ChevronDown } from 'lucide-react'
+import { useDebounceValue } from 'usehooks-ts'
 
 const CreateProductPage = () => {
   const [form] = Form.useForm()
+
+  const [searchCategoty, setSearchCategoty] = useState<string>('')
+  const [debouncedSearchCategory] = useDebounceValue(searchCategoty, 1000)
 
   const onFinish = (values: any) => {
     console.log('Form values:', values)
   }
 
+  // call api get categories
+  const { data: categories } = useGetCategoriesQuery({
+    limit: 10,
+    search: debouncedSearchCategory
+  })
+
+  // useCallback/useMemo
+  const categorieOptions = useMemo(
+    () =>
+      categories?.docs.map((category) => ({
+        value: category._id,
+        label: category.name
+      })) || [],
+    [categories?.docs]
+  )
+  console.log('🚀 ~ CreateProductPage ~ categorieOptions:', categorieOptions)
+
+  const handleSearchCategory = useCallback((value: string) => {
+    setSearchCategoty(value)
+  }, [])
+
   return (
-    <Form form={form} layout='vertical' onFinish={onFinish} style={{ maxWidth: 800, margin: '0 auto' }}>
+    <Form form={form} layout='vertical' onFinish={onFinish}>
       <Row gutter={16}>
         <Col span={12}>
           <Form.Item name='name' label='Tên SP' rules={[{ required: true, message: 'Vui lòng nhập tên sản phẩm' }]}>
@@ -20,22 +49,31 @@ const CreateProductPage = () => {
             <Switch />
           </Form.Item>
         </Col>
-      </Row>
 
-      <Row gutter={16}>
         <Col span={12}>
           <Form.Item name='price' label='Giá' rules={[{ required: true, message: 'Vui lòng nhập giá' }]}>
-            <InputNumber style={{ width: '100%' }} />
+            <InputNumber style={{ width: '100%' }} placeholder='Nhập giá' />
           </Form.Item>
         </Col>
         <Col span={12}>
           <Form.Item name='discountPrice' label='Giảm giá'>
-            <InputNumber style={{ width: '100%' }} />
+            <InputNumber style={{ width: '100%' }} placeholder='Nhập giảm giá' />
           </Form.Item>
         </Col>
-      </Row>
 
-      <Row gutter={16}>
+        <Col span={12}>
+          <Form.Item name='discountPrice' label='Giảm giá'>
+            <Select
+              showSearch
+              placeholder='Chọn danh mục'
+              filterOption={(input, option) => (option?.label ?? '').toLowerCase().includes(input.toLowerCase())}
+              options={categorieOptions}
+              suffixIcon={<ChevronDown />}
+              onSearch={handleSearchCategory}
+            />
+          </Form.Item>
+        </Col>
+
         <Col span={24}>
           <Form.Item name='description' label='Mô tả'>
             <Input.TextArea rows={4} placeholder='Nhập mô tả sp' />
